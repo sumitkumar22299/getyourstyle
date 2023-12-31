9c41e5f827e71b5c7d4d7360723e57d031648487
Sumit <sumit.kumar9@indiamart.com>
2023-12-31 10:55:50 +0530

    Test

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express'); 
 const app = express(); 
-app.use(express.static('public'));
-// app.get('/', function (req, res) { 
-//      res.send('Hello World!'); 
-// }); 
+
+app.get('/', function (req, res) { 
+    res.send('Welcome Sumit'); 
+}); 
+
+app.get('/FortnieReq', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+  
 app.listen(3000, function () { 
      console.log('Example app listening on port 3000!'); 
 }); 
