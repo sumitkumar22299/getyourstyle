b8d7a6be80978fec2614f5d3b1ac39a6daf14361
Sumit <sumit.kumar9@indiamart.com>
2023-12-31 11:15:15 +0530

    Update

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express'); 
-const app = express(); 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 80; // Set the default port to 80
 
-app.get('/', function (req, res) { 
-    res.send('Welcome Sumit'); 
-}); 
+app.get('/', function (req, res) {
+  res.send('Welcome Sumit');
+});
 
 app.get('/FortnieReq', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
-  
-app.listen(3000, function () { 
-     console.log('Example app listening on port 3000!'); 
-}); 
- 
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
